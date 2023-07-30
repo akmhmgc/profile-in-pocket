35b1e3ed7a4c26c206c387402ca937df1d357ece
akmhmgc <akimuhamaguchi@icloud.com>
2023-07-30 20:32:27 +0900

    fix: Fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'log_in' => 'sessions#new', as: :login
-  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+  get 'log-in' => 'sessions#new', as: :login
+  get 'log-out', to: 'sessions#destroy', as: 'log_out'
 
   resources :sessions, only: %i[create destroy]
   resources :profiles, except: %i[index destroy], param: :url_key
