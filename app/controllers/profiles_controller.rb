946c1313d269733a85910facc2ea1c90902d005e
akmhmgc <akimuhamaguchi@icloud.com>
2023-07-30 11:50:08 +0900

    feat: Add profile new page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,24 @@
 class ProfilesController < ApplicationController
   skip_before_action :check_logged_in, only: :show
 
+  def new
+    profile = current_user.profile
+    redirect_to edit_profile_path(profile.user.issued_url.url_key) if profile.present?
+
+    @profile = Profile.new
+    puts @profile
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      redirect_to profile_path(@profile.user.issued_url.url_key)
+    else
+      render :new
+    end
+  end
+
   def show
     url_key = params.require(:url_key)
     issued_url = IssuedUrl.find_by(url_key:)
@@ -11,5 +29,11 @@ class ProfilesController < ApplicationController
 
     user = issued_url.user
     @profile = user.profile
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :description, :twitter_id, :github_id, :qiita_id, :zenn_id, :qiita_id)
   end
 end
